96f91dd3956e0dc85c4f8b8e454ef5b187d7ab01
Koichi Sasada <ko1@atdot.net>
2015-07-03 17:49:51 +0900

    add GC info and restore stdout

--- lib/class_stat/setup.rb
+++ lib/class_stat/setup.rb
@@ -3,35 +3,46 @@ require 'pp'
 
 END{
   file = ENV['CLASS_STAT_FILE']
-  $stdout =  open(file, 'w') if file
+  begin
+    if file
+      prev_stdout = $stdout
+      $stdout = open(file, 'w')
+    end
 
-  iclass, methods = ClassStat.stat
-  total_dup = 0
-  total_iclasses = 0
-  total_methods = 0
+    iclass, methods = ClassStat.stat
+    total_dup = 0
+    total_iclasses = 0
+    total_methods = 0
 
-  iclass.map{|k, v|
-    [k, v, methods[k] * v]
-  }.sort_by{|(k, n, t)|
-    [-t, k.to_s]
-  }.each{|(k, v, t)|
-    total_dup += t
-    total_iclasses += v
-    puts "#{k}\t#{v}\t#{t}"
-  }
-  puts '-- '
+    iclass.map{|k, v|
+      [k, v, methods[k] * v]
+    }.sort_by{|(k, n, t)|
+      [-t, k.to_s]
+    }.each{|(k, v, t)|
+      total_dup += t
+      total_iclasses += v
+      puts "#{k}\t#{v}\t#{t}"
+    }
+    puts '-- '
 
-  methods.sort_by{|k, v|
-    [-v, k.to_s]
-  }.each{|k, v|
-    total_methods += v
-    puts "#{k}\t#{v}"
-  }
+    methods.sort_by{|k, v|
+      [-v, k.to_s]
+    }.each{|k, v|
+      total_methods += v
+      puts "#{k}\t#{v}"
+    }
+    puts '-- '
 
-  puts '-- '
-  puts "total_klasses\t#{methods.size}"
-  puts "total_included\t#{iclass.size}"
-  puts "total_iclasses\t#{total_iclasses}"
-  puts "total_methods\t#{total_methods}"
-  puts "total_dup\t#{total_dup}"
+    pp GC.stat
+    pp ObjectSpace.count_objects
+    puts '-- '
+
+    puts "total_klasses\t#{methods.size}"
+    puts "total_included\t#{iclass.size}"
+    puts "total_iclasses\t#{total_iclasses}"
+    puts "total_methods\t#{total_methods}"
+    puts "total_dup\t#{total_dup}"
+  ensure
+    $stdout = prev_stdout if prev_stdout
+  end
 }
